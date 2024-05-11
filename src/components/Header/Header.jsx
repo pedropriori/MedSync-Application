beaea1c495b19fff7491c8d4bf95c0ea6786e16e
Pedro Priori <kadilo1@hotmail.com>
2024-05-10 22:49:15 -0300

    using auth and dynamic header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useContext } from 'react'
 import logo from '../../assets/images/logo.png'
-import userImg from '../../assets/images/avatar-icon.png'
 import { NavLink, Link } from 'react-router-dom'
 import { BiMenu } from 'react-icons/bi'
+import { authContext } from '../../context/AuthContext'
 
 const navLinks = [
   {
@@ -26,6 +26,7 @@ const navLinks = [
 const Header = () => {
   const headerRef = useRef(null)
   const menuRef = useRef(null)
+  const {user, role, token} = useContext(authContext)
 
   const handleStickyHeader = () => {
     window.addEventListener('scroll', () => {
@@ -77,19 +78,30 @@ const Header = () => {
 
           {/* menu de navegação direito */}
           <div className='flex items-center gap-4'>
-            <div className='hidden'>
-              <Link to='/'>
-              <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
-                <img src={userImg} className='w-full rounded-full' alt='' />
-              </figure>
-              </Link>
-            </div>
-
-            <Link to='/login'>
-              <button className='bg-primaryBgColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>
-                Login
-              </button>
-            </Link>
+            {token && user 
+              ? 
+                <div>
+                  <Link to={`${
+                      role=='doctor'
+                      ? '/doctors/profile/me'
+                      : 'users/profile/me'
+                    }`}
+                  >
+                    <figure className='w-[40px] h-[40px] rounded-full cursor-pointer'>
+                      <img 
+                        src={user?.photo}
+                        className='w-full rounded-full'
+                        alt='user-photo' />
+                    </figure>
+                  </Link>
+                </div>
+              :
+                <Link to='/login'>
+                  <button className='bg-primaryBgColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>
+                    Login
+                  </button>
+                </Link>
+            }
 
             <span className='md:hidden' onClick={toggleMenu}>
               <BiMenu className='w-6 h-6 cursor-pointer' />
