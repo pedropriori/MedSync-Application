d054bea6ad24b3bfbe6a778591fb21e77f06de8b
Pedro Priori <kadilo1@hotmail.com>
2024-05-12 19:00:38 -0300

    dynamic header avatar photo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,72 +1,75 @@
-import { useEffect, useRef, useContext } from 'react'
-import logo from '../../assets/images/logo.png'
-import { NavLink, Link } from 'react-router-dom'
-import { BiMenu } from 'react-icons/bi'
-import { authContext } from '../../context/AuthContext'
+import { useEffect, useRef, useContext } from "react";
+import avatar from "../../assets/images/doctor-img03.png";
+import logo from "../../assets/images/logo.png";
+import { NavLink, Link } from "react-router-dom";
+import { BiMenu } from "react-icons/bi";
+import { authContext } from "../../context/AuthContext";
 
 const navLinks = [
   {
-    path: '/home',
-    display:'Home'
+    path: "/home",
+    display: "Home",
   },
   {
-    path: '/doctors',
-    display:'Encontre um profissional'
+    path: "/doctors",
+    display: "Encontre um profissional",
   },
   {
-    path: '/services',
-    display:'Serviços'
+    path: "/services",
+    display: "Serviços",
   },
   {
-    path: '/contact',
-    display:'Contato'
+    path: "/contact",
+    display: "Contato",
   },
-]
+];
 
 const Header = () => {
-  const headerRef = useRef(null)
-  const menuRef = useRef(null)
-  const {user, role, token} = useContext(authContext)
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+  const { user, role, token } = useContext(authContext);
 
   const handleStickyHeader = () => {
-    window.addEventListener('scroll', () => {
-      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        headerRef.current.classList.add('sticky__header')
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
       } else {
-        headerRef.current.classList.remove('sticky__header')
+        headerRef.current.classList.remove("sticky__header");
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
-    handleStickyHeader()
+    handleStickyHeader();
 
-    return () => window.removeEventListener('scroll', handleStickyHeader)
+    return () => window.removeEventListener("scroll", handleStickyHeader);
   });
 
-  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
 
   return (
-    <header className='header flex items-center' ref={headerRef}>
-      <div className='container'>
-        <div className='flex items-center justify-between'>
+    <header className="header flex items-center" ref={headerRef}>
+      <div className="container">
+        <div className="flex items-center justify-between">
           {/* logo */}
           <div>
-            <img src={logo} alt=''/>
+            <img src={logo} alt="" />
           </div>
 
           {/* menu de navegação */}
-          <div className='navigation' ref={menuRef} onClick={toggleMenu}>
-            <ul className='menu flex items-center gap-[2.7rem]'>
-              {
-                navLinks.map((link, index) => (
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+            <ul className="menu flex items-center gap-[2.7rem]">
+              {navLinks.map((link, index) => (
                 <li key={index}>
-                  <NavLink 
-                    to={link.path} 
-                    className={navClass => 
-                      navClass.isActive 
-                        ? 'text-primaryBgColor text-[16px] leading-7 font-[600]' 
-                        : 'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'
+                  <NavLink
+                    to={link.path}
+                    className={(navClass) =>
+                      navClass.isActive
+                        ? "text-primaryBgColor text-[16px] leading-7 font-[600]"
+                        : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"
                     }
                   >
                     {link.display}
@@ -77,40 +80,55 @@ const Header = () => {
           </div>
 
           {/* menu de navegação direito */}
-          <div className='flex items-center gap-4'>
-            {token && user 
-              ? 
-                <div>
-                  <Link to={`${
-                      role=='doctor'
-                      ? '/doctors/profile/me'
-                      : 'users/profile/me'
-                    }`}
-                  >
-                    <figure className='w-[40px] h-[40px] rounded-full cursor-pointer'>
-                      <img 
-                        src={user?.photo}
-                        className='w-full rounded-full'
-                        alt='user-photo' />
-                    </figure>
-                  </Link>
-                </div>
-              :
-                <Link to='/login'>
-                  <button className='bg-primaryBgColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>
-                    Login
-                  </button>
+          <div className="flex items-center gap-4">
+            {token && user ? (
+              <div>
+                <Link
+                  to={`${
+                    role == "doctor"
+                      ? "/doctors/profile/me"
+                      : "users/profile/me"
+                  }`}
+                >
+                  <figure className="w-[40px] h-[40px] rounded-full cursor-pointer">
+                    {/* === if user dont upload a photo, use a default ==== */}
+                    {user?.photo ? (
+                      <img
+                        src={user.photo}
+                        className="w-full rounded-full"
+                        alt=""
+                      />
+                    ) : (
+                      <img
+                        src={avatar}
+                        alt=""
+                        className="w-full rounded-full"
+                      />
+                    )}
+                    {/* // <img
+                    //   src={user?.photo}
+                    //   className="w-full rounded-full"
+                    //   alt=""
+                    // /> */}
+                  </figure>
                 </Link>
-            }
+              </div>
+            ) : (
+              <Link to="/login">
+                <button className="bg-primaryBgColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
+                  Login
+                </button>
+              </Link>
+            )}
 
-            <span className='md:hidden' onClick={toggleMenu}>
-              <BiMenu className='w-6 h-6 cursor-pointer' />
+            <span className="md:hidden" onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
             </span>
           </div>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
