7571516eca8643d5bb94a2bc4ab277b81b921897
Pedro Priori <kadilo1@hotmail.com>
2024-05-12 19:31:01 -0300

    implementing doctor profile settings

--- src/Dashboard/doctor-account/Profile.jsx
+++ src/Dashboard/doctor-account/Profile.jsx
@@ -1,9 +1,129 @@
+import { useState } from "react";
+
 const Profile = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    bio: "",
+    gender: "",
+    specialization: "",
+    ticketPrice: 0,
+    qualifications: [],
+    experiences: [],
+    timeSlots: [],
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
       <h2 className="text-headingColor font-bold text-[24px] leading-9 mb-10">
         Informações do Perfil
       </h2>
+
+      <form>
+        <div className="mb-5">
+          <p className="form__label">Nome*</p>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            placeholder="Nome Completo"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Email*</p>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="email@example.com"
+            className="form__input"
+            readOnly
+            aria-readonly
+            disabled="true"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Telefone*</p>
+          <input
+            type="number"
+            name="phone"
+            value={formData.phone}
+            onChange={handleInputChange}
+            placeholder="(00) 0 0000-0000"
+            className="form__input"
+          />
+        </div>
+        <div className="mb-5">
+          <p className="form__label">Bio*</p>
+          <input
+            type="text"
+            name="bio"
+            value={formData.bio}
+            onChange={handleInputChange}
+            placeholder="Biografia"
+            className="form__input"
+            maxLength={100}
+          />
+        </div>
+
+        <div className="mb-5">
+          <div className="grid grid-cols-3 gap-5 mb-[30px]">
+            <div>
+              <p className="form__label">Gênero*</p>
+              <select
+                name="gender"
+                value={formData.gender}
+                onChange={handleInputChange}
+                className="form__input py-3.5"
+              >
+                <option value="">Selecione</option>
+                <option value="male">Masculino</option>
+                <option value="female">Feminino</option>
+                <option value="others">Outros</option>
+              </select>
+            </div>
+            <div>
+              <p className="form__label">Especialização*</p>
+              <select
+                name="specialization"
+                value={formData.specialization}
+                onChange={handleInputChange}
+                className="form__input py-3.5"
+              >
+                <option value="">Selecione</option>
+                <option value="cardiologist">Cardiologista</option>
+                <option value="dermatologist">Dermatologista</option>
+                <option value="endocrinologist">Endocrinologista</option>
+                <option value="psychologist">Psicólogo</option>
+                <option value="other">Outro</option>
+              </select>
+            </div>
+
+            <div>
+              <p className="form__label">Preço*</p>
+              <input
+                type="number"
+                placeholder="100"
+                name="ticketPrice"
+                value={formData.ticketPrice}
+                className="form__input"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mb-5">
+          <p className="form__label">Qualificações*</p>
+        </div>
+      </form>
     </div>
   );
 };
