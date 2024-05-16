93c6df535107b3a5f0a40f2d4948c914f2179136
Pedro Priori <kadilo1@hotmail.com>
2024-05-16 10:38:59 -0300

    first step for side panel booking

--- src/pages/Doctors/SidePanel.jsx
+++ src/pages/Doctors/SidePanel.jsx
@@ -1,93 +1,68 @@
+/* eslint-disable react/prop-types */
+import { toast } from "react-toastify";
+import { BASE_URL, token } from "../../../config";
+import convertTime from "../../utils/convertTime";
 
+const SidePanel = ({ doctorId, ticketPrice, timeSlots }) => {
+  const bookingHandler = async () => {
+    try {
+      const res = await fetch(
+        `${BASE_URL}/bookings/checkout-session/${doctorId}`,
+        {
+          method: "post",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
-const SidePanel = () => {
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.message + "Por favor, tente novamente");
+      }
+
+      if (data.session.url) {
+        window.location.href = data.session.url;
+      }
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+
   return (
-    <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
-      <div className='flex items-center justify-between'>
-        <p className='text__para mt-0 font-semibold'>
-          Preço da Consulta
-        </p>
-        <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold'>
-          R$499,99
+    <div className="shadow-panelShadow p-3 lg:p-5 rounded-md">
+      <div className="flex items-center justify-between">
+        <p className="text__para mt-0 font-semibold">Preço da Consulta</p>
+        <span className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold">
+          R${ticketPrice}
         </span>
       </div>
 
       <div className="mt-[30px]">
-        <p className='text__para mt-0 font-semibold text-headingColor'>
+        <p className="text__para mt-0 font-semibold text-headingColor">
           Horários Disponíveis:
         </p>
-        <ul className='mt-3'>
-          <li className='flex items-center justify-between mb-2'>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              Domingo
-            </p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              Fechado
-            </p>
-          </li>
-
-          <li className='flex items-center justify-between mb-2'>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              Segunda-Feira
-            </p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              8:00 - 18:00
-            </p>
-          </li>
-
-          <li className='flex items-center justify-between mb-2'>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              Terça-Feira
-            </p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              8:00 - 18:00
-            </p>
-          </li>
-
-          <li className='flex items-center justify-between mb-2'>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              Quarta-Feira
-            </p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              8:00 - 18:00
-            </p>
-          </li>
-
-          <li className='flex items-center justify-between mb-2'>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              Quinta-Feira
-            </p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              8:00 - 18:00
-            </p>
-          </li>
-
-          <li className='flex items-center justify-between mb-2'>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              Sexta-Feira
-            </p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              8:00 - 18:00
-            </p>
-          </li>
-
-          <li className='flex items-center justify-between mb-2'>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              Sábado
-            </p>
-            <p className='text-[15px] leading-6 text-textColor font-semibold'>
-              Fechado
-            </p>
-          </li>
-
+        <ul className="mt-3">
+          {timeSlots?.map((item, index) => (
+            <li key={index} className="flex items-center justify-between mb-2">
+              <p className="text-[15px] leading-6 text-textColor font-semibold">
+                {item.day.charAt(0).toUpperCase() + item.day.slice(1)}
+              </p>
+              <p className="text-[15px] leading-6 text-textColor font-semibold">
+                {convertTime(item.startingTime)} -{" "}
+                {convertTime(item.endingTime)}
+              </p>
+            </li>
+          ))}
         </ul>
       </div>
 
-      <button className='btn px-2 w-full rounded-md'>
+      <button onClick={bookingHandler} className="btn px-2 w-full rounded-md">
         Agendar Consulta
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default SidePanel
+export default SidePanel;
