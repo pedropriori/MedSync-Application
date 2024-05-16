fe88d64c7c1f5e8de242cfe578930a2a76a2ef98
Pedro Priori <kadilo1@hotmail.com>
2024-05-16 10:34:02 -0300

    create checkout success page

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,40 +1,46 @@
-import Home from '../pages/Home'
-import Services from '../pages/Services'
-import Login from '../pages/Login'
-import SignUp from '../pages/Signup'
-import Contact from '../pages/Contact'
-import Doctors from '../pages/Doctors/Doctors'
-import DoctorDetails from '../pages/Doctors/DoctorDetails'
-import { Routes, Route } from 'react-router-dom'
-import MyAccount from '../Dashboard/user-account/MyAccount'
-import Dashboard from '../Dashboard/doctor-account/Dashboard'
-import ProtectedRoute from './ProtectedRoute'
+import Home from "../pages/Home";
+import Services from "../pages/Services";
+import Login from "../pages/Login";
+import SignUp from "../pages/Signup";
+import Contact from "../pages/Contact";
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
+import { Routes, Route } from "react-router-dom";
+import MyAccount from "../Dashboard/user-account/MyAccount";
+import Dashboard from "../Dashboard/doctor-account/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+import CheckoutSuccess from "../pages/Doctors/CheckoutSuccess";
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/home' element={<Home/>} />
-      <Route path='/doctors' element={<Doctors/>} />
-      <Route path='/doctors/:id' element={<DoctorDetails/>} />
-      <Route path='/login' element={<Login/>} />
-      <Route path='/register' element={<SignUp/>} />
-      <Route path='/contact' element={<Contact/>} />
-      <Route path='/services' element={<Services/>} />
-      <Route path='/users/profile/me' 
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/checkout-success" element={<CheckoutSuccess />} />
+      <Route
+        path="/users/profile/me"
         element={
-          <ProtectedRoute allowedRoles={['patient']} >
-            <MyAccount/>
+          <ProtectedRoute allowedRoles={["patient"]}>
+            <MyAccount />
           </ProtectedRoute>
-        } />
-      <Route path='/doctors/profile/me' 
+        }
+      />
+      <Route
+        path="/doctors/profile/me"
         element={
-          <ProtectedRoute allowedRoles={['doctor']}>
-            <Dashboard/>
+          <ProtectedRoute allowedRoles={["doctor"]}>
+            <Dashboard />
           </ProtectedRoute>
-        } />
+        }
+      />
     </Routes>
-  )
-}
+  );
+};
 
-export default Routers
+export default Routers;
